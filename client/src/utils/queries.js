57355a00f4504c58a5280a030c3dea4ce3d8826b
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-11-29 21:03:32 -0600

    deploy half-finished work

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,6 +2,19 @@ import { gql } from '@apollo/client';
 
 export const GET_ME = gql `
 query me($username: String!, $_id: ID!) {
-    
+    user(_id: $_id){
+    _id
+    username
+    email
+    savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+    }
+}
+
 } 
 `
